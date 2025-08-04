fc60c3934e160d48929be4b1b561ddb33dbae490
Tur-Ochir Ulziibadrakh <96538515+Tur-Ochir@users.noreply.github.com>
2025-08-04 22:58:37 +0800

    Project init

---
+++ Assets/Resource.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Outline outline;
+        
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+}
